6315a549a61430760e48131a033f0c814ed71e63
RobertoPatino1 <rapatino@espol.edu.ec>
2021-11-18 18:51:45 -0500

    Se agrego la clase Persona con su constructor

---
+++ Modelo/Persona.java
@@ -0,0 +1,39 @@
+package Modelo;
+public class Persona{
+    private String nombre;
+    private String direccion;
+    private String telefono; //Puede comenzar con 0
+    private Ciudad ciudad;
+    private String email;
+    
+
+    //Constructor
+
+    public Persona(String nombre, String direccion, String telefono, Ciudad ciudad, String email){
+        //Inicializa los atributos
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.ciudad = ciudad;
+        this.email = email;
+    }
+
+
+    //Getters
+    public String getNombre(){
+        return nombre;
+    }
+    public String getDireccion(){
+        return direccion;
+    }
+    public String getTelefono(){
+        return telefono;
+    }
+    public Ciudad getCiudad(){
+        return ciudad;
+    }
+    public String getEmail(){
+        return email;
+    }
+
+}
