d1639139d4c08624bd1f44f53da5100d2372579e
josbsala <josbsala@espol.edu.ec>
2021-11-24 00:16:53 -0500

    creacion de clase general para las listas de reguistro

---
+++ Modelo/SistemaLista.java
@@ -0,0 +1,14 @@
+package Modelo;
+import java.util.ArrayList;
+//import Modelo.SistemaGeneral;
+
+public class SistemaLista {
+    private ArrayList<SistemaLista> lista;
+    
+    //Métodos 
+    public void reguistrar() {
+        //debe resibir un obtejo de sistema y se ira agregando a una lista vacia 
+        //debe retornar una lista 
+        //lista.add();
+    } 
+}
