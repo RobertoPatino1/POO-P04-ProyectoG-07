f8060f580b3fbb3adcd86e798756c144e915414a
josbsala <josbsala@espol.edu.ec>
2021-11-24 00:14:56 -0500

    se agrega el contructor a la clase Ciudad

--- Modelo/Ciudad.java
+++ Modelo/Ciudad.java
@@ -3,5 +3,9 @@ package Modelo;
 public class Ciudad {
     private String nombre;
     private String provincia;
-    
+
+    public Ciudad(String nombre,String provincia){
+        this.nombre = nombre;
+        this.provincia = provincia;
+    }    
 }
