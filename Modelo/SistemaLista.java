79feb591cb0bf5742241264a394e2f7cec2342ee
josbsala <josbsala@espol.edu.ec>
2021-11-27 12:16:41 -0500

    Creación de metodos de reguistro y visualizacion en Sistema Lista (básico)

--- Modelo/SistemaLista.java
+++ Modelo/SistemaLista.java
@@ -5,10 +5,68 @@ import java.util.ArrayList;
 public class SistemaLista {
     private ArrayList<SistemaLista> lista;
     
-    //Métodos 
-    public void reguistrar() {
-        //debe resibir un obtejo de sistema y se ira agregando a una lista vacia 
-        //debe retornar una lista 
-        //lista.add();
+    //Métodos de Registro 
+    public ArrayList<Mascota> reguistrarMascotas(Mascota m) {
+        ArrayList<Mascota> mascotas = new ArrayList<>(); 
+        mascotas.add(m);
+        return mascotas;
     } 
+    public ArrayList<DuenioMascota> reguistrarDuenioMascota(DuenioMascota dm) {
+        ArrayList<DuenioMascota> duenios = new ArrayList<>();
+        duenios.add(dm);
+        return duenios;
+    }
+    public ArrayList<Ciudad> reguistrarCiudad(Ciudad c) {
+        ArrayList<Ciudad> ciudades = new ArrayList<>();
+        ciudades.add(c);
+        return ciudades;
+    }
+    public ArrayList<Auspiciante> reguistrarAuspiciante(Auspiciante a) {
+        ArrayList<Auspiciante> auspiciantes = new ArrayList<>();
+        auspiciantes.add(a);
+        return auspiciantes;
+    }
+
+    //Metodos de Visualizacion 
+    public void VerListaMascotas(ArrayList<Mascota> mascotas){
+        for(Mascota m:mascotas){
+            m.toString();
+        }
+    }
+    public void VerListaDuenioMascota(ArrayList<DuenioMascota> duenios){
+        //------------------------------
+        /*
+        for(DuenioMascota dm:duenios){
+            dm.toString();
+        }
+        */
+        //------------------------------
+        //no tiene un toString
+        //no tiene gets
+    }
+    public void VerCiudades(ArrayList<Ciudad> ciudades){
+        //------------------------------
+        /*
+        for(Ciudad c:ciudades){
+            c.toString();
+            c.getNombre();
+        }
+        */
+        //------------------------------
+        //no tiene un toString
+        //no tiene gets
+    }
+    public void VerAuspiciantes(ArrayList<Auspiciante> auspiciantes){
+        //------------------------------
+        /*
+        for(Auspiacinte a:auspiciantes){
+            a.toString();
+            a.getNombre();
+        }
+        */
+        //------------------------------
+        //no tiene un toString
+        //no tiene gets     
+    }
+
 }
