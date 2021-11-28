125cb200d33cbf7fc1447e5e420d278f75aec53d
RobertoPatino1 <rapatino@espol.edu.ec>
2021-11-27 22:28:56 -0500

    Se colocaron metodos para manipular los atributos booleanos agregados a la clase concurso, junto con 2 metodos propios del concurso llamados cerrarInscripciones y finalizarConcurso

--- Modelo/Concurso.java
+++ Modelo/Concurso.java
@@ -8,13 +8,14 @@ public class Concurso {
     private String fechaCierreInscripcion;
     private Ciudad ciudad;
     private String lugar;
-    private Premio premio; //Completar la clase premio
+    private Premio premio; 
     private Auspiciante auspiciante;
-    private TipoAnimal dirigidoA;
+    private TipoAnimalesConcurso dirigidoA;
     private boolean estaAbierto; //True si el concurso esta abierto/ false si esta cerrado
     private boolean concursoFinalizado; //True si el concurso ha terminado
+    
 
-    public Concurso(String nombre, String fechaEvento, String horaEvento, String fechaInicioInscripcion, String fechaCierreInscripcion, Ciudad ciudad, String lugar, Premio premio, Auspiciante auspiciante, TipoAnimal dirigidoA){
+    public Concurso(String nombre, String fechaEvento, String horaEvento, String fechaInicioInscripcion, String fechaCierreInscripcion, Ciudad ciudad, String lugar, Premio premio, Auspiciante auspiciante, TipoAnimalesConcurso dirigidoA){
         
         this.nombre= nombre;
         this.fechaEvento= fechaEvento;
@@ -26,10 +27,60 @@ public class Concurso {
         this.premio= premio;
         this.auspiciante= auspiciante;
         this.dirigidoA= dirigidoA;
+        this.estaAbierto = true; //El concurso esta abierto para inscripciones
+        this.concursoFinalizado = false; //El concurso aun no ha terminado
     }
 
-    //Metodo visualizar las mascotas inscritas (imcompleto)
-    public void visualizarMascotasInscritas(){
-        //MascotaInscrita = new ArrayList<Mascota>()
+    //Constructor solo con el nombre
+    public Concurso(String nombre){
+        this.nombre = nombre;
     }
+
+
+    public void cerrarInscripcionesConcurso(){
+        estaAbierto = false;
+    }
+
+    public void finalizarConcurso(){
+        concursoFinalizado = true;
+    }
+
+    //Getters
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public boolean getEstaAbierto(){
+        return estaAbierto;
+    }
+    public boolean getConcursoFinalizado(){
+        return concursoFinalizado;
+    }
+
+    //Metodo equals
+    public boolean equals(Object o){
+        if(this == o){
+            return true;
+        }
+        if(o!=null&& o instanceof Concurso){
+            //Downcasting
+            Concurso concursoEquals = (Concurso) o;
+            return this.nombre.equals(concursoEquals.nombre);
+        }
+        return false;
+    }
+
+
+    //Metodo toString
+    public String toString(){
+        return "Nombre: "+nombre+"|"+"Fecha: "+fechaEvento+"|"+"Hora: "+horaEvento+"|"+"Lugar: "+lugar+"|"+"Dirigido a: "+dirigidoA;
+    }
+
+
+
+   
+
+
+
 }
