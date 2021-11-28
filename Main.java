d12e39f2e0805f2e486a86687a5d1c1d12798a5f
josbsala <josbsala@espol.edu.ec>
2021-11-28 14:34:41 -0500

    Correccion y creaccion de objetos establecidocos en mascotas y ciudades reguistradas en lista

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+import Modelo.Auspiciante;
 import Modelo.Ciudad;
 import Modelo.Concurso;
 import Modelo.DuenioMascota;
@@ -27,7 +28,7 @@ public class Main {
 
 
         */
-        Main.crearObjetos(); // IMPORTANTES COMPROBAR SI ESTO ESTA BIEN 
+        //Main.crearObjetos(); // IMPORTANTES COMPROBAR SI ESTO ESTA BIEN 
 
         Scanner sc = new Scanner(System.in);
         int seleccion;
@@ -141,26 +142,50 @@ public class Main {
 
         //concurso vigente
         System.out.println("---Creacion de Concurso vigente---");
-        SistemaLista.registrarConcurso(SistemaLista.crearConcurso());
+        Ciudad ciudadv = new Ciudad("Guayaquil","Guayas");
+        Auspiciante auspiciantev = new Auspiciante("Raul.Salazar","Base.naval.sur.Av25","0991279217",ciudadv,"rausala@espol.edu.ec","http.a");
+        Concurso concursoVigente = new Concurso("OrejasLargas","30/11/2021","13:24","28/11/2021","10/12/2021",ciudadv,"Guiayaquil","ParqueSamanes","Croquetas",auspiciantev,TipoAnimal.PERRO,TipoAnimalesConcurso.PERROS);
+        SistemaLista.registrarConcurso(concursoVigente);
+
         //creacion de 10 mascotas 
         System.out.println("---Ingreso de Mascotas---");
-        for (int i=0;i<10;i++){
-            SistemaLista.registrarMascotas(SistemaLista.crearMascota());
-        }
+        Mascota mascota1 = new Mascota("Pinky",TipoAnimal.PERRO,"Salchica","24/08/2005","http1",DuenioMascota dm,"2005");
+        SistemaLista.registrarMascotas(mascota1);
+        Mascota mascota2 = new Mascota("Balto",TipoAnimal.PERRO,"Huzky","14/03/1933","http2",DuenioMascota dm,"1933");
+        SistemaLista.registrarMascotas(mascota2);
+        Mascota mascota3 = new Mascota("Krypto",TipoAnimal.PERRO,"Samoyendo","02/03/1955","http3",DuenioMascota dm,"0210");
+        SistemaLista.registrarMascotas(mascota3);
+        Mascota mascota4 = new Mascota("Goofy",TipoAnimal.PERRO,"Aberdeen","17/09/1967","http4",DuenioMascota dm,"1967");
+        SistemaLista.registrarMascotas(mascota4);
+        Mascota mascota5 = new Mascota("Scooby",TipoAnimal.PERRO,"Danes","13/05/1969","http5",DuenioMascota dm,"1969");
+        SistemaLista.registrarMascotas(mascota5);
+        Mascota mascota6 = new Mascota("Garfiel",TipoAnimal.GATO,"Birmano","19/06/1966","http6",DuenioMascota dm,"1966");
+        SistemaLista.registrarMascotas(mascota6);
+        Mascota mascota7 = new Mascota("Silvestre",TipoAnimal.GATO,"Silvestris","14/05/1945","http7",DuenioMascota dm,"0103");
+        SistemaLista.registrarMascotas(mascota7);
+        Mascota mascota8 = new Mascota("Garry",TipoAnimal.GATO,"caracol","23/10/1955","http8",DuenioMascota dm,"1955");
+        SistemaLista.registrarMascotas(mascota8);
+        Mascota mascota9 = new Mascota("Doraemon",TipoAnimal.GATO,"Birmano","01/12/1969","http9",DuenioMascota dm,"1969");
+        SistemaLista.registrarMascotas(mascota9);
+        Mascota mascota10 = new Mascota("Chiquito",TipoAnimal.GATO,"Romano","24/08/2005","http10",DuenioMascota dm,"2000");
+        SistemaLista.registrarMascotas(mascota10);
+
         //creacion de 10 dueños 
         System.out.println("---Ingreso de Dueños---");
-        for (int i=0;i<10;i++){
-            SistemaLista.registrarDuenioMascota(SistemaLista.crearDuenio());
-        }
+        SistemaLista.registrarDuenioMascota();
+
         //creaccion de 3 ciudades
         System.out.println("---Ingreso de ciudades---");
-        for (int i=0;i<3;i++){
-            SistemaLista.registrarCiudad(SistemaLista.crearCiudad());
-        }      
+        Ciudad ciudad1 = new Ciudad("Guayaquil","Guayas");
+        SistemaLista.registrarCiudad(ciudad1);  
+        Ciudad ciudad2 = new Ciudad("Quito","Pichincha");
+        SistemaLista.registrarCiudad(ciudad2); 
+        Ciudad ciudad3 = new Ciudad("Cuenca","Azuay");
+        SistemaLista.registrarCiudad(ciudad3); 
+
         //creacion de 3 auspiciantes
         System.out.println("---Ingreso de Auspiciantes---");
-        for (int i=0;i<3;i++){
-            SistemaLista.registrarAuspiciante(SistemaLista.crearAuspiciante());
+        SistemaLista.registrarAuspiciante();
         }
         
     }
