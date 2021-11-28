503dc4225abf02ff3bbb9f5ff14d10a6d7c5041e
RobertoPatino1 <rapatino@espol.edu.ec>
2021-11-28 01:42:17 -0500

    Se estilizo el metodo main agregando las llamadas a los metodos, y se boraron algunos comentarios de referencias pasadas. Aun es necesario la creacion de los objetos predefinidos y agregar estos a las listas dentro del metodo crearObjetos definido al final de la clase

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 
 import Modelo.Ciudad;
+import Modelo.Concurso;
 import Modelo.DuenioMascota;
 import Modelo.Mascota;
 import Modelo.SistemaLista;
 import Modelo.TipoAnimal;
+import Modelo.TipoAnimalesConcurso;
 import Modelo.SistemaMenu;
 public class Main {
     public static void main(String[] args) {
@@ -36,60 +38,42 @@ public class Main {
             seleccion = SistemaMenu.menuPrincipal();
             switch (seleccion){
                 case 1: //Administrar concursos
+                    SistemaLista.visualizarListaConcursos(); //Se muestra la lista de concursos
                     seleccion1 = SistemaMenu.menuConcursos();
-
-                    //Se debe hacer un print mostrando los concursos existentes
-                    //System.out.println(listaConcursos);
-
-
-
                     if (seleccion == 1){
                         //Creacion de un concurso
                         System.out.println("---Creacion de concurso---");
-
-
-                        
-                        //PEDIR DATOS PARA CREAR EL CONCURSO
-
+                        SistemaLista.crearConcurso();
                         break;
+
                     }else if(seleccion == 2){
                         //Inscripcion de participante
                         System.out.println("---Inscripcion de participante---");
-
-                        //PEDIR DATOS PARA INSCRIBIR AL PARTICIPANTE
-
+                        SistemaLista.inscribirParticipantes();
                         break;
                     }else if (seleccion == 3){
                         //Regresar al menu principal
                         System.out.println("---Regresar al menu principal---");
-                        //---Volver a poner el metodo en esta linea---?
                         break;
                     }
                     break;
 
 
                 case 2: //Administrar Duenios
+                    //Se muestra por pantalla la lista de duenios
+                    SistemaLista.visualizarListaDuenios();
                     seleccion2 = SistemaMenu.menuDuenios();
                     if (seleccion2 == 4){
-                        //Crear duenio
-                        //System.out.println(listaDuenios);
-
-                        SistemaLista.visualizarListaDuenios();
                         System.out.println("---Crear duenio---");
-
                         DuenioMascota duenioCreado = SistemaLista.crearDuenio(); //Se crea el duenio de la mascota
+
                         //Luego se agrega este duenio creado a la lista de duenios
-
                         SistemaLista.registrarDuenioMascota(duenioCreado);
-
-
                         break;
                     }else if(seleccion2 == 5){
                         //Editar duenio
-                        System.out.println("Editar duenio");
-                        
+                        System.out.println("---Editar duenio---");
                         SistemaLista.editarDuenio();
-
                         break;
 
                     }else if(seleccion2 == 6){
@@ -100,24 +84,24 @@ public class Main {
                     break;
 
                 case 3:
+                    //Se muestra la lista de mascotas existentes
+                    SistemaLista.visualizarListaMascotas();
                     seleccion3 = SistemaMenu.menuMascotas();
                     if (seleccion3 == 7){
-
-                        //Se muestra la lista de mascotas existentes
-                        SistemaLista.visualizarListaMascotas();
                         //Crear mascota
-                        System.out.println("Crear mascota");
+                        System.out.println("---Crear mascota---");
                         Mascota mascotaCreada = SistemaLista.crearMascota();
-
                         //Se agrega la mascota a la lista
                         SistemaLista.registrarMascotas(mascotaCreada);
                         break;
+        
                     }else if(seleccion3 == 8){
                         //Eliminar mascota
-                        System.out.println("Eliminar mascota");
+                        System.out.println("---Eliminar mascota---");
                         SistemaLista.eliminarMascota();
                         
                         break;
+                        
                     }else if(seleccion3 == 9){
                         //Regresar al menu
                         System.out.println("Regresar al menu principal");
@@ -139,18 +123,19 @@ public class Main {
 
 
 
+    }
 
-
-
-
-
-
-
-
-
-
-
-
+    public static void crearObjetos(){
+        //Dentro de este metodo se deben crear los objetos especificados al final del problema
+        /*
+        ● 1 Concurso pasado, con 5 mascotas inscritas y 3 ganadores.
+        ● 1 Concurso vigente
+        ● 10 mascotas
+        ● 10 dueños de mascotas
+        ● Las ciudades Guayaquil, Quito y Cuenca
+        ● 3 auspiciantes
+        */
+        
     }
 
 
